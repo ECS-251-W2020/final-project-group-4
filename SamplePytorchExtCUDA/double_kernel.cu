d79cae40c1e01535b2b6f60f8e7b4c07155485fd
Xiaorui Wang <416165938@qq.com>
2020-02-24 20:07:30 -0800

    Add some change to sample and a README file

--- SamplePytorchExtCUDA/double_kernel.cu
+++ SamplePytorchExtCUDA/double_kernel.cu
@@ -1,13 +1,13 @@
 #include <cuda_runtime.h>
 
-__global__ void double_kernel(const float* data, float* output, size_t n) {
+__global__ void double_kernel(const float* data, float* output, size_t n) {  //CUDA function
     const int idx = blockIdx.x * blockDim.x + threadIdx.x;
     if (idx < n) {
         output[idx] = data[idx] * 2;
     }
 }
 
-void launch_double_ext_cuda_kernel(const float* data, float* output, size_t n){
+void launch_double_ext_cuda_kernel(const float* data, float* output, size_t n){ //CPP function to call CUDA function above
     const int threads = 256;
     const int blocks = (n + threads - 1) / threads;
     double_kernel<<<blocks, threads>>>(data, output, n);
