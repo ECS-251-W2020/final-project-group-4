1f45dce6384f84295f4cab7926795a41249b83f3
Xiaorui Wang <416165938@qq.com>
2020-03-09 00:03:46 -0700

    fix demo comment and modify README file

--- PytorchAegis/README.md
+++ PytorchAegis/README.md
@@ -13,11 +13,19 @@
 ## Usage:
 
 1. Build [AegisEngine](../AegisEngine/) in PreRelease mode
+   * Make sure the Windows [SDK of Intel SGX](https://software.intel.com/en-us/sgx/sdk) with version 2.X 2.6.100.2 is installed
+   * Open [AegisEngin.sln](../AegisEngine/AegisEngine.sln) in Visual Studio 2017
+   * Build two projects in prerelease mode under window x64
 2. Copy the dll and lib all 3 files to [PytorchAegis](./)
+   * `AegisEngine/x64/Prerelease/AegisEngine.dll`
+   * `AegisEngine/x64/Prerelease/AegisEngine.lib`
+   * `AegisEngine/x64/Prerelease/StorageEnclave.signed.dll`
 3. Install PytorchAegis with [setup.py](./setup.py)
+        
+        > python setup.py install
 
 See [test_run.py](./test_run.py) for the interface usage for testing the demo.
 
 ## Clarification
 
-- The demo here is to make sure we can run the whole thing. But some thing needed to make it clear. Practically, we do not touch host memory with the exact data. Here in the `test_run.py`, we load a image from disk to host memory and then copy to GPU memory, however, we won't do that since the training data in main memory from user should be encrpyted.
+- The demo here is to make sure we can run the whole thing. But some thing needed to make it clear. Practically, we do not touch host memory with the exact data. Here in the [test_run.py](./test_run.py), we load a image from disk to host memory and then copy to GPU memory, however, we won't do that since the training data in main memory from user should be encrpyted.
--- PytorchAegis/test_run.py
+++ PytorchAegis/test_run.py
@@ -6,30 +6,30 @@ pytorch_aegis.set_aegis_key(x)
 key = pytorch_aegis.get_aegis_key_cuda()
 
 # Demo for an array
-'''
+
 data = torch.randint(0, 200, (2048,), dtype=torch.uint8).cuda()
 for i in range(2048):
     data[i] = i % 200
 edata = pytorch_aegis.encrypt_data(data, key)
 dedata = pytorch_aegis.decrypt_data(edata, key)
-'''
 
-# Demo for an image
-import image_to_numpy
-data = image_to_numpy.load_image_file("my_file.jpg")
-data = torch.as_tensor(data)
-datasize = data.size()
-# data = data.reshape(-1).cuda()
-data = data.cuda()   # The pytorch_aegis is compatible with normal matrix, not just for array
-edata = pytorch_aegis.encrypt_data(data, key)
-dedata = pytorch_aegis.decrypt_data(edata, key)
 
-# Save the decrypted data of image for comparison
-imgt = torch.tensor(dedata,device = 'cpu')
-imgt = imgt.reshape(datasize)
-import matplotlib.pyplot as plt
-plt.imshow(imgt)
-plt.savefig(fname = 'my_file_de.jpg', format = 'jpg')
+# # Demo for an image
+# import image_to_numpy
+# data = image_to_numpy.load_image_file("my_file.jpg")
+# data = torch.as_tensor(data)
+# datasize = data.size()
+# # data = data.reshape(-1).cuda()
+# data = data.cuda()   # The pytorch_aegis is compatible with normal matrix, not just for array
+# edata = pytorch_aegis.encrypt_data(data, key)
+# dedata = pytorch_aegis.decrypt_data(edata, key)
+
+# # Save the decrypted data of image for comparison
+# imgt = torch.tensor(dedata,device = 'cpu')
+# imgt = imgt.reshape(datasize)
+# import matplotlib.pyplot as plt
+# plt.imshow(imgt)
+# plt.savefig(fname = 'my_file_de.jpg', format = 'jpg')
 
 # Print the result 
 print("key", key)
